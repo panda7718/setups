f22ab407ca6d96c0eb206f63fcc57c6dca56dc74
panda7718 <panda7718>
2022-06-09 05:43:15 +0000

    add mhddos_proxy_setup.sh

---
+++ mhddos_proxy_setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/workspace && \
+rm -rf mhddos_proxy && \
+git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git && \
+cd mhddos_proxy && \
+python3 -m pip install -r requirements.txt && \
+python3 -m pip install uvloop
