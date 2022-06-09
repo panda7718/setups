0031860c3e9d52afe6e5ab92d4c14e24e6b00164
panda7718 <panda7718>
2022-06-09 06:56:52 +0000

    n/a

--- proxy_finder/install.sh
+++ proxy_finder/install.sh
@@ -3,7 +3,6 @@
 mkdir -p ~/workspace && \
 cd ~/workspace && \
 rm -rf proxy_finder && \
-sudo apt install --upgrade python3 python3-pip git -y && \
 git clone https://github.com/porthole-ascend-cinnamon/proxy_finder && \
-cd ~/proxy_finder && \
+cd ~/workspace/proxy_finder && \
 python3 -m pip install -r requirements.txt
