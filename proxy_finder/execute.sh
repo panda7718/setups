f9083d003db1106db38af0baef177af3d3b79afb
panda7718 <panda7718>
2022-06-10 13:33:49 +0000

    na

--- proxy_finder/execute.sh
+++ proxy_finder/execute.sh
@@ -1,2 +1,1 @@
-cd ~/workspace/proxy_finder && \
-python3 finder.py --threads 5000 >> nohup.out 2>>nohup.out
+cd ~/workspace/proxy_finder && python3 finder.py --threads 5000 >> nohup.out 2>>nohup.out
