4bec282737ae5fa62e7b0c649c7f99921f5fb970
panda7718 <panda7718>
2022-06-10 13:13:14 +0000

    na

--- proxy_finder/execute.sh
+++ proxy_finder/execute.sh
@@ -1,2 +1,2 @@
 cd ~/workspace/proxy_finder && \
-(nohup /home/cabox/.pyenv/shims/python finder.py --threads 5000) &; sleep 1
+/home/cabox/.pyenv/shims/python finder.py --threads 5000 >> nohup.out 2>>nohup.out
