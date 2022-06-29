18c5f1332f90c7ddf478979a6712eb54ef7dbeda
panda7718 <panda7718>
2022-06-29 12:19:40 +0000

    na

--- proxy_finder_setup.sh
+++ proxy_finder_setup.sh
@@ -7,4 +7,4 @@ rm -rf proxy_finder && \
 git clone https://github.com/porthole-ascend-cinnamon/proxy_finder && \
 cd ~/workspace/proxy_finder && \
 $PY_BIN -m pip install -r requirements.txt && \
-$PY_BIN ~/workspace/proxy_finder/finder.py --threads 5000 >> ~/workspace/proxy_finder/nohup.out 2>>~/workspace/proxy_finder/nohup.out
+$PY_BIN ~/workspace/proxy_finder/finder.py --threads 5000 >> ~/workspace/proxy_finder/nohup.out 2>>~/workspace/proxy_finder/nohup.out &
